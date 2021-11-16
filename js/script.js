f10dd741e70f2547306a7f81829321cabde72108
TonyBrb <anthonybarbagallo95@gmail.com>
2021-11-16 12:21:13 +0100

    visualizzazione dinamica della conversazione col contatto selezionato con @click

---
+++ js/script.js
@@ -0,0 +1,103 @@
+
+const app = new Vue ({
+  el: '#app',
+  data:{
+  contacts: [
+  {
+                    user: 'Michele',
+                    profile_image: 'avatar_1.jpg',
+                    visible: true,
+                    messages: [{
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Ricordati di dargli da mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            text: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+  {
+                    user: 'Fabio',
+                    profile_image: 'avatar_2.jpg',
+                    visible: true,
+                    messages: [{
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            text: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'received'
+                        }
+                    ],
+                },
+{
+                    user: 'Samuele',
+                    profile_image: 'avatar_3.jpg',
+                    visible: true,
+                    messages: [{
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            text: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            text: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+  {
+                    user: 'Luisa',
+                    profile_image: 'avatar_6.jpg',
+                    visible: true,
+                    messages: [{
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ]
+                }
+  ],
+  active_user: -1,
+  isActive: false
+  },
+
+  methods:{
+    showMessages(index){
+      console.log(index);
+      this.active_user=index;
+      this.isActive=true;
+      console.log(active_user);
+    }
+  }
+});
